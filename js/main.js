fcc55ecf3922ec6bfc2471533e1f6dcdc5f1249f
Eduar Carvajal <carvajaldiazeduar@gmail.com>
2014-06-10 23:31:41 -0500

    pruebas

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by usuario on 10/06/14.
+ */
+
+/**
+ * Class Main
+ * @type {{}}
+ */
+var Hamebus = {
+
+    /**
+     * Muestra las noticias de hoy
+     */
+    noticias: function() {
+        /*$.ajax({
+            url:
+        });*/
+    }
+}
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    Habemus.noticias();
+}
